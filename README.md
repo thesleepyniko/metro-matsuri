84b18d6b29322dae23a75e80f25b845107704fb1
Cid <137782638+thisiscid@users.noreply.github.com>
2025-08-24 23:14:28 -0400

    updated builds, added new screen, readme and stuff

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Do you think you can deal with transportation? Fight through different
 districts, season by season, in this rougelike transportation game
 that takes place all around Japan!
+
+# Builds
+
+All web builds are located in the builds folder!
--- main/scripts/main.gd
+++ main/scripts/main.gd
@@ -1,7 +1,7 @@
 extends Node
 
-var screens = ["res://main/scenes/menu.tscn", "res://main/scenes/game.tscn", "res://main/scenes/failed.tscn"]
-enum enum_screens {menu=0, game=1, failed=2}
+var screens = ["res://main/scenes/menu.tscn", "res://main/scenes/game.tscn", "res://main/scenes/failed.tscn", "res://main/scenes/new_page_confirm.tscn"]
+enum enum_screens {menu=0, game=1, failed=2, new_page_confirm=3}
 var curr_state # we want to know what state it is...duh...
 var prev_state
 var debug # set by the user to see any
@@ -33,10 +33,13 @@ func _handle_event_change(action, data):
 				print("starting game")
 			"PlayAgain":
 				change_state(enum_screens.game) 
+			"Understand":
+				change_state(enum_screens.menu)
 	if action == "failed":
 		change_screen(enum_screens.failed)
 	if action == "exit_game":
 		change_screen(enum_screens.menu)
+		
 			
 
 func _on_bus_event(action, data):
@@ -46,7 +49,7 @@ func _on_bus_event(action, data):
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Bus.connect("event", Callable(self, "_on_bus_event"))
-	change_state(enum_screens.menu)
+	change_state(enum_screens.new_page_confirm)
 
 
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
